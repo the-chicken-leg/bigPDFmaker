81f76dcf9848d21509a44e7651d6c68af3d175e9
the-chicken-leg <michael.koch3@pm.me>
2026-03-29 16:45:08 +0200

    change workflow

--- main.py
+++ main.py
@@ -5,34 +5,32 @@ from tkinter.filedialog import askdirectory
 import writers
 
 def main():
-    input("Press Enter key to select a folder.")
-    input_directory = askdirectory(mustexist=True)
-    if not input_directory:
-        return
-    else:
-        input_directory = Path(input_directory)
-        print(f"Selected folder:        {input_directory}")
+    input_directory = None
+    while not input_directory:
+        input("Press Enter key to select a folder.")
+        input_directory = askdirectory(mustexist=True)
+    input_directory = Path(input_directory)
+    print(f"Selected folder:        {input_directory}\n")
 
     get_filename_and_save(input_directory)
 
 def get_filename_and_save(input_directory: Path):
-    input("\nPress Enter key to select save location.")
-    output_path = asksaveasfilename(
-            defaultextension="pdf",
-            filetypes=[("PDF files", "*.pdf"), ("All Files", "*.*")],
-    )
-    if not output_path:
-        return
-    else:
-        output_path = Path(output_path)
-        print(f"Selected save location: {output_path}")
+    output_path = None
+    while not output_path:
+        input("Press Enter key to select save location.")
+        output_path = asksaveasfilename(
+                defaultextension="pdf",
+                filetypes=[("PDF files", "*.pdf"), ("All Files", "*.*")],
+        )
+    output_path = Path(output_path)
+    print(f"Selected save location: {output_path}\n")
 
-    print("\nCreating big PDF. This might take a few minutes...")
+    print("Creating big PDF. This might take a few minutes...")
     writer, added_to_big_pdf = writers.create_writer(input_directory)
     writers.write_writer(writer, added_to_big_pdf, output_path)
     # writers.write_writer(writers.compress_writer(writer), added_to_big_pdf, output_path)      # compression broken
     num_files = len(added_to_big_pdf)
-    print(f"\nBig PDF created. {num_files} PDFs included.")
+    print(f"Big PDF created. {num_files} PDFs included.")
 
 if __name__ == "__main__":
     main()
