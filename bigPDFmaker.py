f31ad451108a988cf6e699f71b3bfb3ca842fac4
the-chicken-leg <michael.koch3@pm.me>
2026-04-06 09:22:53 +0200

    remove if name == main

--- bigPDFmaker.py
+++ bigPDFmaker.py
@@ -11,63 +11,59 @@ from tkinter.filedialog import askdirectory
 
 from pypdf import PdfWriter
 
-def main():
-    # get input directory
-    input_directory = None
-    while not input_directory:
-        input("Press Enter key to select a folder.")
-        input_directory = askdirectory(mustexist=True)
-    input_directory = Path(input_directory)
-    print(f"Selected folder:        {input_directory}\n")
+# get input directory
+input_directory = None
+while not input_directory:
+    input("Press Enter key to select a folder.")
+    input_directory = askdirectory(mustexist=True)
+input_directory = Path(input_directory)
+print(f"Selected folder:        {input_directory}\n")
 
-    # get output path
-    output_path = None
-    while not output_path:
-        input("Press Enter key to select save location.")
-        output_path = asksaveasfilename(
-                defaultextension="pdf",
-                filetypes=[("PDF files", "*.pdf"), ("All Files", "*.*")],
-        )
-    output_path = Path(output_path)
-    print(f"Selected save location: {output_path}\n")
-
-    # create writer
-    print("Creating big PDF. This might take a few minutes...")
-
-    glob_sort = sorted(
-        input_directory.glob("*.pdf"),
-        key=lambda filepath: filepath.name.lower(),
+# get output path
+output_path = None
+while not output_path:
+    input("Press Enter key to select save location.")
+    output_path = asksaveasfilename(
+            defaultextension="pdf",
+            filetypes=[("PDF files", "*.pdf"), ("All Files", "*.*")],
     )
-    glob_dedup = {filepath.name: filepath for filepath in glob_sort}    # this doesn't do anything useful now, but would be useful for dedeplication for a future rglob use case
+output_path = Path(output_path)
+print(f"Selected save location: {output_path}\n")
 
-    writer = PdfWriter()
-    added_to_big_pdf = []
-    # for filepath in islice(glob_dedup.values(), 10):    # use for testing slices
-    for filepath in glob_dedup.values():
-        try:
-            current_page = writer.get_num_pages()
-            writer.append(filepath, import_outline=False)
-            writer.add_outline_item(title=filepath.name, page_number=current_page)
-            added_to_big_pdf.append(filepath.name + "\n")
-        except Exception as err:
-            print(f"Error adding {filepath.name} to big PDF: {err}")
-            continue
+# create writer
+print("Creating big PDF. This might take a few minutes...")
 
-    # compress writer (broken)
-    # for page in writer.pages:
-    #     page.compress_content_streams(level=9)
-    # writer.compress_identical_objects(remove_identicals=True, remove_orphans=True)
+glob_sort = sorted(
+    input_directory.glob("*.pdf"),
+    key=lambda filepath: filepath.name.lower(),
+)
+glob_dedup = {filepath.name: filepath for filepath in glob_sort}    # this doesn't do anything useful now, but would be useful for dedeplication for a future rglob use case
 
-    # write writer
-    with output_path.open(mode="wb") as output_file:
-        writer.write(output_file)
-        
-    contents_output_path = output_path.parent / f"{output_path.name}_contents.txt"
-    with contents_output_path.open(mode="w", encoding="utf-8") as output_file:
-        output_file.writelines(added_to_big_pdf)
+writer = PdfWriter()
+added_to_big_pdf = []
+# for filepath in islice(glob_dedup.values(), 10):    # use for testing slices
+for filepath in glob_dedup.values():
+    try:
+        current_page = writer.get_num_pages()
+        writer.append(filepath, import_outline=False)
+        writer.add_outline_item(title=filepath.name, page_number=current_page)
+        added_to_big_pdf.append(filepath.name + "\n")
+    except Exception as err:
+        print(f"Error adding {filepath.name} to big PDF: {err}")
+        continue
 
-    num_files = len(added_to_big_pdf)
-    print(f"Big PDF created. {num_files} PDFs included.")
+# compress writer (broken)
+# for page in writer.pages:
+#     page.compress_content_streams(level=9)
+# writer.compress_identical_objects(remove_identicals=True, remove_orphans=True)
 
-if __name__ == "__main__":
-    main()
+# write writer
+with output_path.open(mode="wb") as output_file:
+    writer.write(output_file)
+    
+contents_output_path = output_path.parent / f"{output_path.name}_contents.txt"
+with contents_output_path.open(mode="w", encoding="utf-8") as output_file:
+    output_file.writelines(added_to_big_pdf)
+
+num_files = len(added_to_big_pdf)
+print(f"Big PDF created. {num_files} PDFs included.")
